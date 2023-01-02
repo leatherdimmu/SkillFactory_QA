c536d22330d92c41b13da61c3e579ffe4a5c3d77
Khodkov Dmitrii <120494328+leatherdimmu@users.noreply.github.com>
2023-01-02 20:16:15 +0300

    Update 13.8.19.py

--- 13.8.19.py
+++ 13.8.19.py
@@ -1,18 +1,22 @@
-age = int(input('Введите Ваш возраст- '))
-kol_vo_tiket = int(input('Введите количество билетов, которые Вы хотите приобрести на мероприятие- '))
+tikets = int(input('Введите количество билетов, которые Вы хотите приобрести на мероприятие- '))
 prise_1 = 990
 prise_2 = 1390
-if kol_vo_tiket < 3:
-  if age < 18:
-    print('Проходите на конференцию бесплатно')
-  elif age >= 18 and age <= 25:
-    print("К оплате", prise_1, "рублей")
-  elif age > 25 or age <= 100:
-    print("К оплате", prise_2, "рублей")
-else:
-  if age < 18:
-    print('Проходите на конференцию бесплатно')
-  elif age >= 18 and age <= 25:
-    print("К оплате", prise_1 * kol_vo_tiket * 0.9 , "рублей")
-  elif age > 25 or age <= 100:
-    print("К оплате", prise_2 * kol_vo_tiket * 0.9 , "рублей")
+prise_itog = 0
+for i in range(tikets): 
+  if tikets < 4:
+    age = int(input('Введите Ваш возраст- '))
+    if age < 18:
+      prise_itog = prise_itog + 0
+    elif age >= 18 and age <= 25:
+      prise_itog = prise_itog + prise_1
+    elif age > 25 or age <= 100:
+      prise_itog = prise_itog + prise_2
+  elif tikets >= 4:
+    age = int(input('Введите Ваш возраст- '))
+    if age < 18:
+      prise_itog = prise_itog + 0
+    elif age >= 18 and age <= 25:
+      prise_itog = prise_itog + prise_1 * 0.9
+    elif age > 25 or age <= 100:
+      prise_itog = prise_itog + prise_2 * 0.9
+print("Итого к оплате -", prise_itog) 
